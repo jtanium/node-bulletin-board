8a57c99d57d6903b8045e61f60664fa5e381f9e3
Jason Edwards <jtanium@gmail.com>
2020-06-12 11:35:38 -0600

    Added tag variable.

--- bulletin-board-app/terraform/main.tf
+++ bulletin-board-app/terraform/main.tf
@@ -1,9 +1,9 @@
 
-module "linux_tweet_app" {
+module "node_bulletiin_board" {
   source  = "my.scalr.com/env-stst3lhp1fhvffo/scalr_example/aws"
   version = "0.1.0"
 
-  app_image = "jasonpayroc/node-bulliten-board"
+  app_image = "jasonpayroc/node-bulletin-board:${var.tag}"
   dns_name = "${var.instance_name}.payroc.com"
   instance_name = var.instance_name
   instance_port = 8080
